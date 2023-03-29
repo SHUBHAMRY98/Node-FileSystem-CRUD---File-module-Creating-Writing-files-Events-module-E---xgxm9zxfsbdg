73a8a333bf8883b1e25ed95cf11381ca38358638
SHUBHAMRY98 <106675186+SHUBHAMRY98@users.noreply.github.com>
2023-03-30 04:56:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
 const fs = require('fs/promises')
 
-const myFileWriter = async ('File.txt',' Hello') => {
-	
-	// write code here
-	// dont chnage function name
+const myFileWriter = async (fileName, fileContent) => {
+	fs.writeFile(fileName,fileContent,(err) => {
+		if(err) throw err;
+		console.log("File has been saved!");
+	});
 }
 
-const myFileReader = async ('File.txt) => {
-	
-	// write code here
-	// dont chnage function name
+const myFileReader = async (fileName) => {
+	const data = await fs.readFile(fileName,'utf8');
+	console.log(data);
+	return data;
 }
 
-
-const myFileUpdater = async ('File.txt',' World') => {
-	
-	// write code here
-	// dont chnage function name
+const myFileUpdater = async (fileName, fileContent) => {
+	fs.appendFile(fileName,fileContent,(err) => {
+		if(err) throw err;
+		console.log("file has been updated")
+	})
 }
 
-const myFileDeleter = async ('File.txt) => {
-	
-	// write code here
-	// dont chnage function name
+const myFileDeleter = async (fileName) => {
+	fs.unlink(fileName,(err) => {
+		if(err) throw err;
+		console.log('File.txt was deleted');
+	});
 }
-
+myFileWriter("File.txt","Hello");
+myFileUpdater('File.txt',' World');
+myFileReader('File.txt');
+myFileDeleter('File.txt');
 
 
 module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
+
+
