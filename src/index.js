b733f8f0197355c80e44d95833411755be9c09ae
SHUBHAMRY98 <106675186+SHUBHAMRY98@users.noreply.github.com>
2023-03-30 04:29:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 const fs = require('fs/promises')
 
 const myFileWriter = async (fileName, fileContent) => {
+	fs.writeFile('File.txt','Hello',(err) =>{
+console.log("created");});
 	// write code here
 	// dont chnage function name
 }
 
 const myFileReader = async (fileName) => {
+	fs.readFile('File.txt','utf-8',(err,data) =>{
+console.log(data);});
 	// write code here
 	// dont chnage function name
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
+	fs.appendFiles('File.txt',' World',(err) =>{
+console.log("appended");});
 	// write code here
 	// dont chnage function name
 }
 
 const myFileDeleter = async (fileName) => {
+	fs.unlink("Newfile.txt",(err) =>{
+console.log("delted");});
 	// write code here
 	// dont chnage function name
 }
